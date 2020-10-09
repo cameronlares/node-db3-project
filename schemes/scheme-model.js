ecaeb5c6472272411a598fe86cd98cebc21a2436
Cameron Lares <cameronlares@gmail.com>
2020-10-08 22:32:12 -0400

    Post req scheme works

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,9 +3,34 @@ const db = require('../data/db-config')
 //resolves to an array of users
 module.exports = {
     find,
+    findById,
+    findSteps,
+    add
 }
 
 function find() {
     return db('schemes')
+}
+
+function findById(id){
+    return db('schemes').where({id}).first();
+}
+
+function findSteps(id){
+    return db('schemes').where({id}).first()
+} 
+
+function add(schemeData){
+    return db('schemes')
+    .insert(schemeData, "id")
+    .then(ids => {
+        const id= ids[0];
+          // all queries return an array,
+            // even if it only has one element
+            // .first() will extract the first element
+            // from the array and return it
+            // return db("users").where({ id }).first();
+return findById(id);
+    })
 }
 
